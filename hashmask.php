9c83875df8b420782ef5d547ef9797fab4c259d8
shamess <shamess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-22 10:51:36 +0000

    Added hashmask to password fields in other parts of WordPress
    git-svn-id: https://plugins.svn.wordpress.org/haskmask-for-wordpress/trunk@156942 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hashmask.php
+++ hashmask.php
@@ -2,27 +2,55 @@
 /**
 Plugin Name: Hashmask for WordPress
 Plugin URI: http://lab.arc90.com/2009/07/hashmask.php
-Description: Adds arc90's hashmask to the admin log in field.
+Description: Adds arc90's hashmask to the admin log in field, and various other password fields
 Author: Shamess
-Version: 0.9
-Author URI: http://shamess.info/business/
+Version: 1.0
+Author URI: http://shamess.info/business/wordpress/customise.php
 */
 
-//  These are required until this ticket ( https://core.trac.wordpress.org/ticket/10630 ) has been applied
-add_action('login_head', 'wp_print_head_scripts', 1);
-add_action('login_head', 'wp_enqueue_scripts', 1);
-
-if('wp-login.php' == substr (basename ($_SERVER[ 'REQUEST_URI' ]), 0, 12)) {
-  wp_register_script ('sparkline', '/wp-content/plugins/hashmaskforwordpress/jquery.sparkline-1.4.2.js', array( 'jquery' ));
-  wp_register_script ('jquery-sha1', '/wp-content/plugins/hashmaskforwordpress/jquery.sha1.js', array( 'jquery' ));
-  wp_register_script ('hashmask', '/wp-content/plugins/hashmaskforwordpress/jquery.hashmask.js', array( 'sparkline', 'jquery-sha1' ));
-  wp_enqueue_script ('hashmask');
+//  Register the scripts we need, and then enqueue them
+function queueScripts () {
+	wp_register_script ('sparkline', '/wp-content/plugins/hashmaskforwordpress/jquery.sparkline-1.4.2.js', array( 'jquery' ));
+	wp_register_script ('jquery-sha1', '/wp-content/plugins/hashmaskforwordpress/jquery.sha1.js', array( 'jquery' ));
+	wp_register_script ('hashmask', '/wp-content/plugins/hashmaskforwordpress/jquery.hashmask.js', array( 'sparkline', 'jquery-sha1' ));
+	wp_enqueue_script ('hashmask');
 }
 
-function add_hashmask_script () {
-  echo "<script language=\"JavaScript\" type=\"text/javascript\">jQuery(document).ready ( function () { jQuery('#user_pass').hashmask(); } );</script>";
+//  We need to do different things depending on which page we're in
+//  Trigger on login page
+if ('wp-login.php' == substr (basename ($_SERVER['REQUEST_URI']), 0, 12)) {
+	queueScripts();
+	
+	//  These are required until this ticket ( https://core.trac.wordpress.org/ticket/10630 ) has been applied
+	add_action('login_head', 'wp_print_head_scripts', 1);
+	add_action('login_head', 'wp_enqueue_scripts', 1);
+	
+	function add_hashmask_script () {
+		echo "<script language=\"JavaScript\" type=\"text/javascript\">
+		jQuery(function () {
+			jQuery('#user_pass').hashmask();
+		});
+		</script>";
+	}
+
+	add_action ('login_head', add_hashmask_script);
+//  Trigger on user profile change pages
+} elseif ('profile.php' == substr (basename ($_SERVER['REQUEST_URI']), 0, 11) || 'user-edit.php' == substr (basename ($_SERVER['REQUEST_URI']), 0, 13)) {
+	queueScripts();
+	
+	function add_hashmask_script () {
+		echo "<script language=\"JavaScript\" type=\"text/javascript\">
+		jQuery(function () {
+			//  Make the new password fields wider
+			jQuery('#pass1, #pass2').attr ('size', 30);
+			jQuery('#pass1, #pass2').hashmask();
+			jQuery('#pass1-jquery-hashmask-sparkline').css ('top', parseInt (jQuery('#pass1-jquery-hashmask-sparkline').css ('top')) - 12);
+			jQuery('#pass2-jquery-hashmask-sparkline').css ('top', parseInt (jQuery('#pass2-jquery-hashmask-sparkline').css ('top')) - 12);
+			jQuery('.indicator-hint').append (' <strong>Also</strong>, if the two images in the password field don\'t match each other, your passwords aren\'t the matching.');
+		});
+		</script>";
+	}
+	
+	add_action ('admin_head', add_hashmask_script);
 }
-
-add_action ('login_head', add_hashmask_script);
-
 ?>
